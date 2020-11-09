ad127fb38dc9fd76044fe3ff637e1c27c8026c1e
Valera <v.drozdovich@intelico.su>
2020-11-09 16:03:59 +0300

    add bubbles

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Blob } from 'react-blob'
 
 const Main = () => {
-    // const { t, i18n } = useTranslation();
-
     return (
-        <h1>Main</h1>
+        <>
+            <h1>Main</h1>
+            <Blob
+                size="25vh"
+                style={{
+                    position: "absolute",
+                    top: "25%%",
+                    right: "75%",
+                    zIndex: -1,
+                    backgroundColor: "#21D4FD",
+                    color: "white",
+                    opacity: 1,
+                    fontSize: "50vh"
+                }}
+                animationDuration="5s"
+            >
+                <i className="fab fa-react"></i>
+            </Blob>
+            <Blob
+                size="25vh"
+                style={{
+                    position: "absolute",
+                    top: "25%",
+                    right: "55%",
+                    zIndex: -1,
+                    backgroundColor: "#21D4FD",
+                    color: "white",
+                    opacity: 1,
+                    fontSize: "50vh"
+                }}
+                animationDuration="10s"
+            >
+                <i className="fab fa-react"></i>
+            </Blob>
+        </>
     )
 }
 
