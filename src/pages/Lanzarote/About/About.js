9b9cc8402549b085ebdd9263fd7c272ec481c315
Valera <v.drozdovich@intelico.su>
2020-11-11 18:10:23 +0300

    add flippy toures

--- src/pages/Lanzarote/About/About.js
+++ src/pages/Lanzarote/About/About.js
@@ -8,38 +8,82 @@ import './About.scss'
 
 const About = () => {
     const { t } = useTranslation();
+
+    const renderBlobs = () => {
+        return (
+            <>
+                {/* Left blobs */}
+                <Blob
+                    size="15vh"
+                    style={{
+                        zIndex: -1,
+                        backgroundColor: "#D5EFFB",
+                        color: "white",
+                        fontSize: "80vh",
+                        position: 'absolute',
+                        left: '12%',
+                        top: '2%'
+                    }}
+                    animationDuration="15s"
+                >
+                    <i className="fab fa-react"></i>
+                </Blob>
+                <Blob
+                    size="32vh"
+                    style={{
+                        zIndex: -1,
+                        backgroundColor: "#D5EFFB",
+                        color: "white",
+                        fontSize: "80vh",
+                        position: 'absolute',
+                        left: '20%'
+                    }}
+                    animationDuration="25s"
+                    animationTimingFunction="ease-in-out"
+                >
+                    <i className="fab fa-react"></i>
+                </Blob>
+
+                {/* Right blobs */}
+                <Blob
+                    size="40vh"
+                    style={{
+                        zIndex: -1,
+                        backgroundColor: "#D5EFFB",
+                        color: "white",
+                        fontSize: "80vh",
+                        position: 'absolute',
+                        top: '15%',
+                        height: '50vh',
+                        right: '10%',
+                    }}
+                    animationDuration="50s"
+                    animationTimingFunction="ease-in-out"
+                >
+                    <i className="fab fa-react"></i>
+                </Blob>
+                <Blob
+                    size="10vh"
+                    style={{
+                        zIndex: -1,
+                        backgroundColor: "#D5EFFB",
+                        color: "white",
+                        fontSize: "80vh",
+                        position: 'absolute',
+                        top: '15%',
+                        right: '30%',
+                    }}
+                    animationDuration="30s"
+                >
+                    <i className="fab fa-react"></i>
+                </Blob>
+            </>
+        )
+    }
+
     return (
         <div className="about">
-            <Blob
-                size="15vh"
-                style={{
-                    zIndex: -1,
-                    backgroundColor: "#D5EFFB",
-                    color: "white",
-                    fontSize: "80vh",
-                    position: 'absolute',
-                    left: '12%',
-                    top: '2%'
-                }}
-                animationDuration="15s"
-            >
-                <i className="fab fa-react"></i>
-            </Blob>
-            <Blob
-                size="32vh"
-                style={{
-                    zIndex: -1,
-                    backgroundColor: "#D5EFFB",
-                    color: "white",
-                    fontSize: "80vh",
-                    position: 'absolute',
-                    left: '20%'
-                }}
-                animationDuration="35s"
-            >
-                <i className="fab fa-react"></i>
-            </Blob>
-
+            {renderBlobs()}
             <div className="container">
                 <h1>About Island</h1>
                 <div className="about__flex">
