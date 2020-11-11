fa09a46d0012f5be85c78d222c2fe4840cc6dc1d
Ihar Ivaniuk <igor@intelico.su>
2020-11-11 18:13:26 +0300

    simple fixes

--- src/pages/Main/Blocs/SecondBlock.js
+++ src/pages/Main/Blocs/SecondBlock.js
@@ -1,54 +1,78 @@
-import React from 'react';
-import { Blob } from 'react-blob'
+import React from "react";
+import { Blob } from "react-blob";
+
+import Button from "../../../components/Buttons/Button";
+import pathicon from "../../../assets/group1.png";
 
 const SecondBlock = ({ t }) => (
-    <div className='main_second__block'>
-        <div className='main_second__block-cols'>
-            <h1 className="main_second__block-title">{t('aboutVidodoGuide')}</h1>
-            <p className="main_second__block-desc">{t('secondBlockDescOne')}</p>
-            <p className="main_second__block-desc">{t('secondBlockDescTwo')}</p>
-            <Blob size="13vh" style={{
-                zIndex: -1,
-                backgroundColor: "#D5EFFB",
-                position: "absolute",
-                left: "30%",
-                top: "0"
-            }} animationDuration="10s">
-                <i className="fab fa-react"></i>
-            </Blob>
-            <Blob size="35vh" style={{
-                zIndex: -1,
-                backgroundColor: "#D5EFFB",
-                position: "absolute",
-                left: "50%",
-                top: "0"
-            }} animationDuration="25s">
-                <i className="fab fa-react"></i>
-            </Blob>
-        </div>
-        <div className='main_second__block-cols'>
-            <p className="main_second__block-desc">{t('secondBlockDescThree')}</p>
-            <p className="main_second__block-desc">{t('secondBlockDescFour')}</p>
-            <Blob size="13vh" style={{
-                zIndex: -1,
-                backgroundColor: "#D5EFFB",
-                position: "absolute",
-                left: "40%",
-                top: "25%"
-            }} animationDuration="25s">
-                <i className="fab fa-react"></i>
-            </Blob>
-            <Blob size="45vh" style={{
-                zIndex: -1,
-                backgroundColor: "#D5EFFB",
-                position: "absolute",
-                left: "70%",
-                top: "40%"
-            }} animationDuration="25s">
-                <i className="fab fa-react"></i>
-            </Blob>
-        </div>
+  <div className="main_second__block">
+    <div className="main_second__block-titlewrapper">
+      <h1 className="main_second__block-title">{t("aboutVidodoGuide")}</h1>
+      <Button icon={pathicon} type="secondary" />
     </div>
+    <div className="main_second__block-cols">
+      <div className="main_second__block-col">
+        <p className="main_second__block-desc">{t("secondBlockDescOne")}</p>
+        <p className="main_second__block-desc">{t("secondBlockDescTwo")}</p>
+        <Blob
+          size="13vh"
+          style={{
+            zIndex: -1,
+            backgroundColor: "#D5EFFB",
+            position: "absolute",
+            left: "30%",
+            top: "0",
+          }}
+          animationDuration="10s"
+        >
+          <i className="fab fa-react"></i>
+        </Blob>
+        <Blob
+          size="35vh"
+          style={{
+            zIndex: -1,
+            backgroundColor: "#D5EFFB",
+            position: "absolute",
+            left: "50%",
+            top: "0",
+          }}
+          animationDuration="25s"
+        >
+          <i className="fab fa-react"></i>
+        </Blob>
+      </div>
+      <div className="main_second__block-col">
+        <p className="main_second__block-desc">{t("secondBlockDescThree")}</p>
+        <p className="main_second__block-desc">{t("secondBlockDescFour")}</p>
+        <Blob
+          size="13vh"
+          style={{
+            zIndex: -1,
+            backgroundColor: "#D5EFFB",
+            position: "absolute",
+            left: "40%",
+            top: "25%",
+          }}
+          animationDuration="25s"
+        >
+          <i className="fab fa-react"></i>
+        </Blob>
+        <Blob
+          size="45vh"
+          style={{
+            zIndex: -1,
+            backgroundColor: "#D5EFFB",
+            position: "absolute",
+            left: "70%",
+            top: "40%",
+          }}
+          animationDuration="25s"
+        >
+          <i className="fab fa-react"></i>
+        </Blob>
+      </div>
+    </div>
+  </div>
 );
 
 export default SecondBlock;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { useTranslation } from "react-i18next";
 
-import FirstBlock from './Blocs/FirstBlock';
-import SecondBlock from './Blocs/SecondBlock';
+import FirstBlock from "./Blocs/FirstBlock";
+import SecondBlock from "./Blocs/SecondBlock";
+import ThitdBlock from "./Blocs/ThitdBlock";
 
 import "./Main.scss";
 
 const Main = () => {
-    const { t } = useTranslation();
-    return (
-        <>
-            <FirstBlock t={t} />
-            <SecondBlock t={t} />
-        </>
-    );
+  const { t } = useTranslation();
+  return (
+    <>
+      <FirstBlock t={t} />
+      <SecondBlock t={t} />
+      <ThitdBlock t={t} />
+    </>
+  );
 };
 
 export default Main;
