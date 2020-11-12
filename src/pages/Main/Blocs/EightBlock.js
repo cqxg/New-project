fe134950c2a68c5e86c0f9f11379b4e93d1878c3
unknown <cqxg@yandex.ru>
2020-11-13 00:27:36 +0300

    feat: add eight block

--- src/pages/Main/Blocs/EightBlock.js
+++ src/pages/Main/Blocs/EightBlock.js
@@ -1,15 +1,38 @@
 import React from "react";
+import { Blob } from "../../../components/Blob/Blob";
 
+import Button from "../../../components/Buttons/Button";
 import bubbleSixBlock from "../../../components/BubbleSixBlock/BubbleSixBlock";
 
+import appStoreIcon from "../../../img/svg/appStore.svg";
+import googlePlay from "../../../img/svg/googlePlay.svg";
+
 const EightBlock = ({ t }) => (
-  <div className="six_block">
+  <div className="eight_block">
     <div className="container">
-      <div className="six_block__flex">
-        <div className="six_block__left">{bubbleSixBlock()}</div>
-        <div className="six_block__right">
-            <h1>{t("eventsSpecialOferTitle")}</h1>
-            <p>{t("eventsSpecialOferDesk")}</p>
+      <div className="eight_block__flex">
+        <div className="eight_block__left">{bubbleSixBlock()}</div>
+        <div className="eight_block__right">
+          <h1>{t("installTheVidodo")}</h1>
+          <p>{t("mainFirstBlocSubtitle")}</p>
+          <div className="eight_block__buttons">
+            <Button
+              icon={appStoreIcon}
+              type="primary"
+              text={t("appStoreBtn")}
+            />
+            <Button
+              icon={googlePlay}
+              type="success"
+              text={t("googlePlayBtn")}
+            />
+          </div>
+          <div className="eight_block__qr">
+            <span className="eight_block__qr-desc">
+              {t("mainFirstBlockQr")}
+            </span>
+            <div className="eight_block__img"></div>
+          </div>
         </div>
       </div>
     </div>
