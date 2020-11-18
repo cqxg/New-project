bc49f32d8746c559a2b5cfddf790786585cedcd2
Valera <v.drozdovich@intelico.su>
2020-11-18 15:17:34 +0300

    bg bubbles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { withRouter } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 
 import { Routes } from './routes/routes';
+import { Blob } from './components/Blob/Blob';
 import Cookies from './components/Cookies/Cookies';
 import Menu from './components/Menu/Menu';
 import Preloader from './components/Preloader/Preloader';
 
 import './App.css';
+import ReactWOW from 'react-wow';
 
 const App = (props) => {
   const [cookies, setCookie] = useCookies(['user']);
@@ -30,14 +32,48 @@ const App = (props) => {
     setTimeout(() => {
       setLoading(false);
       document.getElementsByTagName('body')[0].classList.add('loaded')
-    }, 4000);
+    }, 1);
   }, []);
+
+  const renderBackGroundBubbles = () => {
+    const coordinates = []
+    const maxNum = 5;
+    const max = document.querySelector('.switch-wrapper div:first-child')?.offsetHeight, min = 1000
+    for (let i = 0; i < maxNum; i++) {
+      coordinates.push(Math.floor(Math.random() * (max - min + 1) + min))
+    }
+    return (
+      <>
+        {coordinates.map((num) => {
+          return (
+            <ReactWOW animation='fadeInUp' delay='0.2s'>
+              <div style={{position: 'absolute', left: `${Math.floor(Math.random() * 100 + 1)}%`, top: `${num}px`}} className="animate__animated">
+                <Blob
+                  size={`${Math.floor(Math.random() * 25 + 1)}vh`}
+                  style={{
+                    zIndex: -1,
+                    color: 'white',
+                    fontSize: '80vh',
+                    backgroundColor: '#D1F1FD',
+                  }}
+                  animationDuration="4s"
+                >
+
+                </Blob>
+              </div>
+            </ReactWOW>
+          )
+        })}
+      </>
+    )
+  }
 
   return (
     <>
       {loading ? <Preloader /> : null}
       <Menu changeLocale={changeLocale} activeLocale={activeLocale} theme={locationRoute === '/about' ? 'light' : 'dark'} />
       <Routes />
+      {renderBackGroundBubbles()}
       {toggleCookie && !cookies.user ? <Cookies cookies={cookies} handleCookies={handleCookies} /> : null}
     </>
   );
