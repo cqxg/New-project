4a25aa045715d171ba651ba965aeabddad123283
Valera <v.drozdovich@intelico.su>
2020-11-18 17:57:37 +0300

    add fucking bubbles

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Menu from './components/Menu/Menu';
 import Preloader from './components/Preloader/Preloader';
 
 import './App.css';
+import { bubbleTexture } from "./components/Bubbles/bubbleTexture";
 
 const App = (props) => {
   const [loading, setLoading] = useState(true);
@@ -34,10 +35,20 @@ const App = (props) => {
     }, 1);
   }, []);
 
+  document.addEventListener('wheel', function(e){
+    document.querySelectorAll('.bg__bubbles').forEach((item) => {
+      if (e.deltaY > 0) {
+        item.style.animation = 'translateBubbleonScrollToBottom 30s ease-in-out'
+      } else if(e.deltaY < 0) {
+        item.style.animation = 'translateBubbleonScrollToTop 30s ease-in-out'
+      }
+    })
+  })
+
   const renderBackGroundBubbles = () => {
     const coordinates = []
     const maxNum = 5;
-    const max = document.querySelector('.switch-wrapper div:first-child')?.offsetHeight, min = 1000
+    const max = document.documentElement.clientHeight + window.pageYOffset , min = window.pageYOffset
     for (let i = 0; i < maxNum; i++) {
       coordinates.push(Math.floor(Math.random() * (max - min + 1) + min))
     }
@@ -45,18 +56,13 @@ const App = (props) => {
       <>
         {coordinates.map((num) => {
           return (
-              <div style={{position: 'absolute', left: `${Math.floor(Math.random() * 100 + 1)}%`, top: `${num}px`}} className="animate__animated">
+              <div style={{opacity: 0, animation: 'translateBubbleonScrollToTop 40s ease-in-out', zIndex: 2, position: 'absolute', left: `${Math.floor(Math.random() * 75 + 5)}%`, top: `${num}px`}} className="animate__animated bg__bubbles">
                 <Blob
-                  size={`${Math.floor(Math.random() * 25 + 1)}vh`}
-                  style={{
-                    zIndex: -1,
-                    color: 'white',
-                    fontSize: '80vh',
-                    backgroundColor: '#D1F1FD',
-                  }}
-                  animationDuration="4s"
+                  size={`${Math.floor(Math.random() * 25 + 5)}vh`}
+                  style={{backgroundColor: '#eee',}}
+                  animationDuration="25s"
                 >
-
+                  {bubbleTexture()}
                 </Blob>
               </div>
           )
@@ -74,7 +80,7 @@ const App = (props) => {
         theme={locationRoute === "/about" ? "light" : "dark"}
       />
       <Routes />
-      {/* {renderBackGroundBubbles()} */}
+      {renderBackGroundBubbles()}
       {toggleCookie && !cookies.user ? <Cookies cookies={cookies} handleCookies={handleCookies} /> : null}
     </>
   );
